703c6a74b97fd3cc46ee7965e5fc206455c63ac0
Andrea Alduino <andrealduino05@gmail.com>
2026-04-29 10:26:27 +0200

    chore: add comments to database config files

--- config/database.example.php
+++ config/database.example.php
@@ -1,10 +1,13 @@
 <?php
 
+// Load Composer dependencies (including phpdotenv)
 require_once __DIR__ . '/../vendor/autoload.php';
 
+// Load the .env file from the root of the project
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
 
+// Create the database connection using the .env variables
 $connection = mysqli_connect(
     $_ENV['DB_HOST'],
     $_ENV['DB_USER'],
@@ -12,6 +15,7 @@ $connection = mysqli_connect(
     $_ENV['DB_NAME']
 );
 
+// Check if the connection was successful
 if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
 }
