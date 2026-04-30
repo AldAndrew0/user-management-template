396e0d4dd0e61acf6a707cf2b02e210dd37d14e6
Andrea Alduino <andrealduino05@gmail.com>
2026-04-30 09:31:53 +0200

    feat: add users list page

---
+++ pages/Users/index.php
@@ -0,0 +1,47 @@
+<?php
+require_once '../../config/database.php';
+require_once '../../includes/header.php';
+require_once '../../includes/navbar.php';
+
+// Fetch all users from the database
+$result = mysqli_query($connection, "SELECT * FROM users ORDER BY created_at DESC");
+?>
+
+<main>
+    <div class="container">
+        <div style="display: flex; justify-content: space-between; align-items: center; margin-bottom: 1.5rem;">
+            <h1>Users</h1>
+            <a href="/pages/Users/create.php" class="btn btn-primary">+ Add User</a>
+        </div>
+
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Role</th>
+                    <th>Created At</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php while ($user = mysqli_fetch_assoc($result)): ?>
+                    <tr>
+                        <td><?php echo $user['id']; ?></td>
+                        <td><?php echo $user['name']; ?></td>
+                        <td><?php echo $user['email']; ?></td>
+                        <td><?php echo $user['role']; ?></td>
+                        <td><?php echo $user['created_at']; ?></td>
+                        <td>
+                            <a href="/pages/Users/edit.php?id=<?php echo $user['id']; ?>" class="btn btn-primary">Edit</a>
+                            <a href="/pages/Users/delete.php?id=<?php echo $user['id']; ?>" class="btn btn-danger">Delete</a>
+                        </td>
+                    </tr>
+                <?php endwhile; ?>
+            </tbody>
+        </table>
+    </div>
+</main>
+
+<?php require_once '../../includes/footer.php'; ?>
