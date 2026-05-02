554961885c10c59dadb53378489456ae6ebac863
Andrea Alduino <andrealduino05@gmail.com>
2026-05-02 20:08:35 +0200

    fix: update navbar links for .test domain

--- includes/navbar.php
+++ includes/navbar.php
@@ -2,19 +2,19 @@
 
 <nav>
     <div class="nav-brand">
-        <a href="/user-management-template/">Gestionale Template</a>
+        <a href="/">Gestionale Template</a>
     </div>
 
     <ul class="nav-links">
-        <li><a href="/user-management-template/">Dashboard</a></li>
-        <li><a href="/user-management-template/pages/Users/">Users</a></li>
+        <li><a href="/">Dashboard</a></li>
+        <li><a href="/pages/Users/">Users</a></li>
 
         <?php if (isset($_SESSION['user_id'])): ?>
             <!-- Show logout link if user is logged in -->
-            <li><a href="/user-management-template/pages/auth/logout.php">Logout</a></li>
+            <li><a href="/pages/auth/logout.php">Logout</a></li>
         <?php else: ?>
             <!-- Show login link if user is not logged in -->
-            <li><a href="/user-management-template/pages/auth/login.php">Login</a></li>
+            <li><a href="/pages/auth/login.php">Login</a></li>
         <?php endif; ?>
     </ul>
 </nav>
